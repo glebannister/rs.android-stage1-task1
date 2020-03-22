3d0b134f46bebdf7e84f717a60a9c01d3c4b2e71
Gleb <igi2000@inbox.ru>
2020-03-22 18:28:33 +0300

    feat: stringParser done

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -2,8 +2,61 @@ package subtask4
 
 class StringParser {
 
-    // TODO: Complete the following function
+
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var resultArray = arrayListOf<String>()
+        val stringBuilder = StringBuilder()
+        for (i in inputString.indices) {
+            if (inputString[i] == '[') {
+                var count = 1
+                for (j in i + 1 until inputString.length) {
+                    if (inputString[j] == '[') {
+                        count++
+                    }
+                    if (inputString[j] == ']') {
+                        count--
+                        if (count == 0) {
+                            resultArray.add(stringBuilder.toString())
+                            stringBuilder.clear()
+                            break
+                        }
+                    }
+                    stringBuilder.append(inputString[j])
+                }
+            } else if (inputString[i] == '<') {
+                var count2 = 1
+                for (j in i + 1 until inputString.length) {
+                    if (inputString[j] == '<') {
+                        count2++
+                    }
+                    if (inputString[j] == '>') {
+                        count2--
+                        if (count2 == 0) {
+                            resultArray.add(stringBuilder.toString())
+                            stringBuilder.clear()
+                            break
+                        }
+                    }
+                    stringBuilder.append(inputString[j])
+                }
+            } else if (inputString[i] == '(') {
+                var count3 = 1
+                for (j in i + 1 until inputString.length) {
+                    if (inputString[j] == '(') {
+                        count3++
+                    }
+                    if (inputString[j] == ')') {
+                        count3--
+                        if (count3 == 0) {
+                            resultArray.add(stringBuilder.toString())
+                            stringBuilder.clear()
+                            break
+                        }
+                    }
+                    stringBuilder.append(inputString[j])
+                }
+            }
+        }
+        return resultArray.toTypedArray()
     }
 }
